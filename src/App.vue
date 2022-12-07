353a8ddc4379fb1720510a81ef07d2b1dd9a34f9
Hugo Madureira <hugoomdra@gmail.com>
2022-12-07 15:17:35 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
 
-<h1><img src="./assets/logo-la-rochelle.jpg" style="width: 500px;" alt="LR PARKING"></h1>
+<!-- <h1><img src="./assets/logo-la-rochelle.jpg" style="width: 500px;" alt="LR PARKING"></h1> -->
 
+Test version en ligne
 
 <div v-if="response" v-for="value in response.data.records" style="background-color: #01FFFF; padding: 20px; margin: 10px;">
   <p><span style="font-weight: bold;">{{ value.fields.nom }}</span> : {{ value.fields.nb_places_disponibles }}/{{value.fields.nb_places}}</p>
 </div>
+<div v-else>
+  Si ce message s'affiche, c'est que l'API ne répond pas. Vous pouvez ouvrir la console pour visualiser l'erreur
+</div>
 
-<p>made by mdramedia</p>
+<!-- <p>made by mdramedia</p> -->
 
 </template>
 
@@ -17,7 +21,7 @@ import axios from 'axios'
 import { ref } from 'vue';
 import { values } from './data/data'
 
-const url = "https://opendata.agglo-larochelle.fr/api/records/1.0/search/dataset=parking___places_disponibles_en_temps_reel&api-key=2ac2e9a3-3ba3-4d82-b071-0af0c8bac92c"
+const url = "https://api.agglo-larochelle.fr/production/opendata/api/records/1.0/search/dataset=parking___places_disponibles_en_temps_reel&facet=id&api-key=2ac2e9a3-3ba3-4d82-b071-0af0c8bac92c"
 
 const response = ref()
 
