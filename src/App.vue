8d8260ff7c7c3faeb04647da998886e44b790e44
Hugo Madureira <hugoomdra@gmail.com>
2022-12-07 14:35:34 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
 <h1><img src="./assets/logo-la-rochelle.jpg" style="width: 500px;" alt="LR PARKING"></h1>
 
 
-<div v-for="value in response.data.records" style="background-color: #01FFFF; padding: 20px; margin: 10px;">
-  <p v-if="response" ><span style="font-weight: bold;">{{ value.fields.nom }}</span> : {{ value.fields.nb_places_disponibles }}/{{value.fields.nb_places}}</p>
+<div v-if="response" v-for="value in response.data.records" style="background-color: #01FFFF; padding: 20px; margin: 10px;">
+  <p><span style="font-weight: bold;">{{ value.fields.nom }}</span> : {{ value.fields.nb_places_disponibles }}/{{value.fields.nb_places}}</p>
 </div>
 
 <p>made by mdramedia</p>
@@ -17,7 +17,7 @@ import axios from 'axios'
 import { ref } from 'vue';
 import { values } from './data/data'
 
-const url = "https://api.agglo-larochelle.fr/production/opendata/api/records/1.0/search/dataset=parking___places_disponibles_en_temps_reel&rows=1000&facet=id"
+const url = "https://opendata.agglo-larochelle.fr/api/records/1.0/search/dataset=parking___places_disponibles_en_temps_reel&api-key=2ac2e9a3-3ba3-4d82-b071-0af0c8bac92c"
 
 const response = ref()
 
@@ -29,8 +29,6 @@ async function fetchData() {
   }
   
 }
-
-  
 
 fetchData();
 
